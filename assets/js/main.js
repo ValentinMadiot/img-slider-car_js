19355f902c8e5c1e74ca733f4b64d34bf44f6477
valentinmadiot <valentin.madiot@gmail.com>
2025-06-17 19:53:23 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,10 @@ if (navClose) {
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll(".nav__ link");
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  const navMenu = document.getElementByTd("nav-menu");
+  const navMenu = document.getElementById("nav-menu");
   // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
 };
